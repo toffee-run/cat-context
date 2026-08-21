7abb2b5daa022af2c3f5eee0c127b5a5e91ee84b
toffee35 <nailzagru@gmail.com>
2026-08-21 20:28:56 +0200

    feat: Добавление зависимостей и базовых структур

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use bollard::Docker;
+
+#[derive(Default)]
+enum Base {
+    #[default]
+    Debian,
+    Alpine,
+    Arch,
 }
+
+enum Agent {
+    ClaudeCode,
+    Codex,
+    Antigravity,
+    Opencode,
+}
+
+#[derive(Default)]
+enum Prompt {
+    File(PathBuf),
+    Text(String),
+    #[default]
+    None,
+}
+
+enum Action {
+    Start {
+        base: Base,
+        agent: Agent,
+        prompt: Prompt,
+    },
+    Restart {
+        container: String,
+        agent: Option<Agent>,
+        prompt: Option<Prompt>,
+        save: bool,
+    },
+    Stop {
+        container: String,
+    },
+    Delete {
+        container: String,
+    },
+}
+
+struct Command {
+    connect: Docker,
+    action: Action,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {}
